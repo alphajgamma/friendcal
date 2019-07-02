d0edd3a269338c9d438560c7abdf27b21816fabb
Andrew Greenough <andygreenough@gmail.com>
2019-07-02 12:10:59 +0100

    Initial Commit

---
+++ friendcal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  friendcal
+//
+//  Created by Andrew Greenough on 02/07/2019.
+//  Copyright © 2019 Steam Mode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
